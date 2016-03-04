69ca04f7669dc19ffc18d92122156599f20b2c63
Jean-Philippe <jean-philippe@demarque.com>
2016-03-04 09:53:20 -0500

    Support ECDCA signatures in addition to RSA

--- sign/sign_test.go
+++ sign/sign_test.go
@@ -1,14 +1,47 @@
 package sign
 
 import (
-	"crypto/dsa"
-	"crypto/rand"
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/rsa"
+	"crypto/sha256"
 	"crypto/tls"
+	"math/big"
 	"testing"
 )
 
-func TestSigning(t *testing.T) {
-	cert, err := tls.LoadX509KeyPair("cert/sample.crt", "cert/sample.pem")
+func TestSigningRSA(t *testing.T) {
+	cert, err := tls.LoadX509KeyPair("cert/sample_rsa.crt", "cert/sample_rsa.pem")
+	if err != nil {
+		t.Error("Couldn't load sample certificate ", err)
+		t.FailNow()
+	}
+
+	signer, err := NewSigner(&cert)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	input := map[string]string{"test": "test"}
+	sig, err := signer.Sign(input)
+
+	if expected := "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256"; sig.Algorithm != expected {
+		t.Errorf("Expected '%s', got '%s'", expected, sig.Algorithm)
+	}
+
+	canon, _ := Canon(input)
+	hashed := sha256.Sum256(canon)
+
+	if privKey, ok := cert.PrivateKey.(*rsa.PrivateKey); ok {
+		if err := rsa.VerifyPKCS1v15(&privKey.PublicKey, crypto.SHA256, hashed[:], sig.Value); err != nil {
+			t.Error("Expected the signature to be valid, got", err)
+		}
+	}
+}
+
+func TestSigningECDSA(t *testing.T) {
+	cert, err := tls.LoadX509KeyPair("cert/sample_ecdsa.crt", "cert/sample_ecdsa.pem")
 	if err != nil {
 		t.Error("Couldn't load sample certificate ", err)
 		t.FailNow()
@@ -26,15 +59,31 @@ func TestSigning(t *testing.T) {
 		t.Error(err)
 	}
 
-	if sig.Algorithm != "http://www.w3.org/2000/09/xmldsig#rsa-sha256" {
-		t.Error("Expected 'http://www.w3.org/2000/09/xmldsig#rsa-sha256', got ", sig.Algorithm)
+	if expected := "http://www.w3.org/2001/04/xmldsig-more#ecdsa-sha256"; sig.Algorithm != expected {
+		t.Errorf("Expected '%s', got '%s'", expected, sig.Algorithm)
+	}
+
+	r, s := getParamsFromECDSASignature(sig.Value)
+
+	canon, _ := Canon(input)
+	hashed := sha256.Sum256(canon)
+
+	if privKey, ok := cert.PrivateKey.(*ecdsa.PrivateKey); ok {
+		if publicKey, ok := privKey.Public().(*ecdsa.PublicKey); ok {
+			if !ecdsa.Verify(publicKey, hashed[:], r, s) {
+				t.Error("Expected the signature to be valid")
+			}
+		}
 	}
 }
 
-func genKey() dsa.PrivateKey {
-	var k dsa.PrivateKey
+func getParamsFromECDSASignature(b []byte) (*big.Int, *big.Int) {
+	half := len(b) / 2
+	r := big.NewInt(0)
+	s := big.NewInt(0)
 
-	dsa.GenerateKey(&k, rand.Reader)
+	r.SetBytes(b[0:half])
+	s.SetBytes(b[half:])
 
-	return k
+	return r, s
 }
