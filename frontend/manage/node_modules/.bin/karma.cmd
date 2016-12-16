c496a4182d2ec5a8901c3ae19405f50355bb92b7
snemegeer <stefaan@epagine.fr>
2016-12-16 11:56:40 +0100

    delete node_modules , start hashed password

--- frontend/manage/node_modules/.bin/karma.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\karma-cli\bin\karma" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\karma-cli\bin\karma" %*
-)
