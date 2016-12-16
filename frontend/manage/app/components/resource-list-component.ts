d925504326aac0b25ff21de1a552a142de8f020f
danielweck <daniel.weck@gmail.com>
2016-12-16 10:47:12 +0000

    styles.css renamed to avoid mistaking with LCP server style.css

--- frontend/manage/app/components/resource-list-component.ts
+++ frontend/manage/app/components/resource-list-component.ts
@@ -13,7 +13,7 @@ import { PurchaseService } from './purchase.service';
     moduleId: module.id,
     selector: 'resources',
     templateUrl: '/app/components/resource-list.html',
-    styleUrls: ['../../app/components/resource.css', '../../style.css'], // from /js/app/components... 
+    styleUrls: ['../../app/components/resource.css', '../../styles.css'], // from /js/app/components... 
     providers: [ResourceService, PurchaseService]
 })
 
