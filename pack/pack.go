e1327de62c3213aae79da4ff519d0fead49131ca
Laurent Le Meur <laurent.lemeur@edrlab.org>
2019-06-17 11:07:14 +0200

    typo correction

--- pack/pack.go
+++ pack/pack.go
@@ -1,4 +1,4 @@
-/ Copyright 2017 European Digital Reading Lab. All rights reserved.
+// Copyright 2017 European Digital Reading Lab. All rights reserved.
 // Licensed to the Readium Foundation under one or more contributor license agreements.
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file exposed on Github (readium) in the project repository.
