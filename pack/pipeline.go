fe2df8f2c78e5542c7660db675abea8eeba4f4b8
Laurent Le Meur <laurent.lemeur@edrlab.org>
2020-01-02 11:38:12 +0100

    Re-apply evolution from JP.Bougie in October

--- pack/pipeline.go
+++ pack/pipeline.go
@@ -1,5 +1,4 @@
-// Copyright 2017 European Digital Reading Lab. All rights reserved.
-// Licensed to the Readium Foundation under one or more contributor license agreements.
+// Copyright 2020 Readium Foundation. All rights reserved.
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file exposed on Github (readium) in the project repository.
 
@@ -11,10 +10,11 @@ import (
 	"encoding/hex"
 	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"time"
 
-	"github.com/satori/go.uuid"
+	uuid "github.com/satori/go.uuid"
 
 	"github.com/readium/readium-lcp-server/crypto"
 	"github.com/readium/readium-lcp-server/epub"
@@ -80,13 +80,14 @@ type Packager struct {
 
 func (p Packager) work() {
 	for t := range p.Incoming {
+		log.Println("Packager working on an incoming EPUB, encryption task")
 		r := Result{}
 		p.genKey(&r)
 		zr := p.readZip(&r, t.Body, t.Size)
 		ep := p.readEpub(&r, zr)
 		encrypted, key := p.encrypt(&r, ep)
-		p.addToStore(&r, encrypted.File)
-		p.addToIndex(&r, key, t.Name, encrypted.Size, encrypted.Sha256, epub.ContentType_EPUB)
+		p.addToStore(&r, encrypted)
+		p.addToIndex(&r, key, t.Name, encrypted, epub.ContentType_EPUB)
 
 		t.Done(r)
 	}
@@ -155,25 +156,26 @@ func (p Packager) encrypt(r *Result, ep epub.Epub) (*EncryptedFileInfo, []byte) 
 	return &encryptedFileInfo, key
 }
 
-func (p Packager) addToStore(r *Result, f *os.File) {
+func (p Packager) addToStore(r *Result, info *EncryptedFileInfo) {
 	if r.Error != nil {
 		return
 	}
 
-	_, r.Error = p.store.Add(r.Id, f)
+	_, r.Error = p.store.Add(r.Id, info.File)
 
-	f.Close()
-	os.Remove(f.Name())
+	info.File.Close()
+	os.Remove(info.File.Name())
 }
 
-func (p Packager) addToIndex(r *Result, key []byte, name string, contentSize int64, contentHash string, contentType string) {
+func (p Packager) addToIndex(r *Result, key []byte, name string, info *EncryptedFileInfo, contentType string) {
 	if r.Error != nil {
 		return
 	}
 
-	r.Error = p.idx.Add(index.Content{r.Id, key, name, contentSize, contentHash, contentType})
+	r.Error = p.idx.Add(index.Content{Id: r.Id, EncryptionKey: key, Location: name, Length: info.Size, Sha256: info.Sha256, Type: contentType})
 }
 
+// NewPackager waits for incoming EPUB files, encrypts them and adds them to the store
 func NewPackager(store storage.Store, idx index.Index, concurrency int) *Packager {
 	packager := Packager{
 		Incoming: make(chan *Task),
