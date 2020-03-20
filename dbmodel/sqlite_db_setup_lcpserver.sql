3bfec39c655ed50f81c3cee7df97b47aa7c5f3aa
Laurent Le Meur <laurent.lemeur@edrlab.org>
2020-03-20 16:26:52 +0100

    Split sqlite scripts, one file per server

---
+++ dbmodel/sqlite_db_setup_lcpserver.sql
@@ -0,0 +1,23 @@
+CREATE TABLE content (
+  id varchar(255) PRIMARY KEY NOT NULL,
+  encryption_key varchar(64) NOT NULL,
+  location text NOT NULL, 
+  length bigint,
+  sha256 varchar(64),
+  "type" varchar(255) NOT NULL DEFAULT 'application/epub+zip'
+);
+
+CREATE TABLE license (
+  id varchar(255) PRIMARY KEY NOT NULL,
+  user_id varchar(255) NOT NULL,
+  provider varchar(255) NOT NULL,
+  issued datetime NOT NULL,
+  updated datetime DEFAULT NULL,
+  rights_print int(11) DEFAULT NULL,
+  rights_copy int(11) DEFAULT NULL,
+  rights_start datetime DEFAULT NULL,
+  rights_end datetime DEFAULT NULL,
+  content_fk varchar(255) NOT NULL,
+  lsd_status integer default 0,
+  FOREIGN KEY(content_fk) REFERENCES content(id)
+);
