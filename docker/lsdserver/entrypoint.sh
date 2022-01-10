40c62657e2f2ce1e9112e82f6d874673188aa540
Pierre Leroux <panacpp@gmail.com>
2022-01-10 18:21:43 +0100

    lsd docker + lcp fixes

--- docker/lsdserver/entrypoint.sh
+++ docker/lsdserver/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-READONLY_BOOL=${READONLY_BOOL=false}
-AES256=${AES256=_GCM}
-READIUM_LCPSERVER_CONFIG=/config.yaml
+READIUM_LSDSERVER_CONFIG=/config.yaml
 
-envsubst < $1 > ${READIUM_LCPSERVER_CONFIG}
+READONLY_BOOL=${READONLY_BOOL=false}
+
+envsubst < $1 > ${READIUM_LSDSERVER_CONFIG}
 
 echo "exec $2"
 
