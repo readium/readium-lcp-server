96757d9b59c7ddb31098fd55a350613a1d991371
Pierre Leroux <panacpp@gmail.com>
2022-07-05 17:45:44 +0200

    up dockerrun

--- docker/dockerrun.sh
+++ docker/dockerrun.sh
@@ -1,29 +1,6 @@
 #! /bin/bash
 
-LCP_BASE_URL="http://127.0.0.1:8091"
-LSD_BASE_URL="http://127.0.0.1:8092"
-FRONTEND_BASE_URL="http://127.0.0.1:8093"
-
-DB_URI="mysql://admin:admin@tcp(db:3306)/readium-lcp"
-MASTER_REPOSITORY="/raw"
-ENCRYPTED_REPOSITORY="/encrypted"
-
-# The usernames and passwords must match the ones in the htpasswd files for each server.
-LSD_NOTIFY_AUTH_USER="adm_username"
-LSD_NOTIFY_AUTH_PASS="adm_password"
-LCP_UPDATE_AUTH_USER="adm_username"
-LCP_UPDATE_AUTH_PASS="adm_password"
-
-S3_ENDPOINTS="http://minio:8082"
-S3_ACCESS_ID="user" #key
-S3_SECRET="12345678"
-S3_DISABLE_SSL_BOOL="true"
-S3_BUCKET="readium-lcp"
-S3_REGION=""
-S3_TOKEN=""
-
 PORT=8080
-
 
 docker kill mylcp
 docker rm mylcp
