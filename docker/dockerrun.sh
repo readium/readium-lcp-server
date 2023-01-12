0a992d963a07254599af5b97981ed35f84245d6e
Pierre Leroux <panacpp@gmail.com>
2023-01-12 16:49:11 +0100

    fix: docker run arg port and volumeName

--- docker/dockerrun.sh
+++ docker/dockerrun.sh
@@ -1,17 +1,24 @@
 #! /bin/bash
 
-PORT=8080
+set -x # expands variable and prints line
 
-docker kill mylcp
-docker rm mylcp
+PORT=${1:-8080}
+VOLUME_NAME=${2:-/tmp/lcp}
+NAME=${3:-mylcp}
+CONTAINER_NAME=${4:-lcpmasterserver}
+
+docker kill $NAME
+docker rm $NAME
 
 # uncomment this line to build
 # ./docker/dockerbuild.sh `pwd` master
 
   # --platform linux/amd64 \
 docker run \
-  --name mylcp \
+  --name $NAME \
   --publish $PORT:$PORT \
   --env PORT=$PORT \
-  --volume /tmp/lcp:/lcp \
-  lcpmasterserver
+  # --env FRONTEND_BASE_URL="http://127.0.0.1:9090/frontend" \
+  # --env BASE_URL="http://127.0.0.1:9090/frontend/" \
+  --volume $VOLUME_NAME:/lcp \
+  $CONTAINER_NAME
