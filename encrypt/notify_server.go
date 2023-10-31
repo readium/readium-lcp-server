16b495d3edacd1bea967dc959a1bd4e44790f7b6
Laurent Le Meur <laurent.lemeur@edrlab.org>
2023-10-31 19:07:31 +0100

    adapt notification to the v2 server and optional cms.

--- encrypt/notify_server.go
+++ encrypt/notify_server.go
@@ -9,12 +9,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
+	"os"
+	"strings"
 	"time"
 
 	apilcp "github.com/readium/readium-lcp-server/lcpserver/api"
 )
 
-type PublicationV2 struct {
+// LCPServerMsgV2 is used for notifying an LCP Server V2
+type LCPServerMsgV2 struct {
 	UUID          string `json:"uuid"`
 	Title         string `json:"title"`
 	EncryptionKey []byte `json:"encryption_key"`
@@ -24,53 +28,95 @@ type PublicationV2 struct {
 	Checksum      string `json:"checksum"`
 }
 
-// NotifyLcpServer notifies the License Server or PubStore of the encryption of newly added publication
-func NotifyLcpServer(pub *apilcp.LcpPublication, notifyURL string, username string, password string, v2 bool) error {
+type Coded struct {
+	Code string `json:"code"`
+}
+
+type Entity struct {
+	Name string `json:"name"`
+}
+
+// CMSMsg is used for notifying a CMS
+// todo: why is this structure imposed by pubstore (Coded, Entity)?
+type CMSMsg struct {
+	UUID            string    `json:"uuid"`
+	Title           string    `json:"title"`
+	DatePublication time.Time `json:"datePublication"`
+	Description     string    `json:"description"`
+	CoverUrl        string    `json:"coverUrl"`
+	Language        []Coded   `json:"language"`
+	Publisher       []Entity  `json:"publisher"`
+	Author          []Entity  `json:"author"`
+	Category        []Entity  `json:"category"`
+}
+
+// NotifyLCPServer notifies the License Server of the encryption of a publication
+func NotifyLCPServer(pub Publication, lcpsv string, v2 bool, username string, password string) error {
 
 	// An empty notify URL is not an error, simply a silent encryption
-	if notifyURL == "" {
-		fmt.Println("No notification sent")
+	if lcpsv == "" {
 		return nil
 	}
 
-	var req *http.Request
-
-	// prepare the call to the endpoint /contents/<id>
-	var jsonBody []byte
+	if !strings.HasPrefix(lcpsv, "http://") && !strings.HasPrefix(lcpsv, "https://") {
+		lcpsv = "http://" + lcpsv
+	}
+	var notifyURL string
 	var err error
-	if !v2 {
-		var urlBuffer bytes.Buffer
-		urlBuffer.WriteString(notifyURL)
-		urlBuffer.WriteString("/contents/")
-		urlBuffer.WriteString(pub.ContentID)
-
-		jsonBody, err = json.Marshal(*pub)
-		if err != nil {
-			return err
-		}
-		req, err = http.NewRequest("PUT", urlBuffer.String(), bytes.NewReader(jsonBody))
-		if err != nil {
-			return err
-		}
-
-		// prepare the call to the endpoint /publications/<id>
+	if v2 {
+		notifyURL, err = url.JoinPath(lcpsv, "publications")
 	} else {
-		url := notifyURL + "/publications/"
+		notifyURL, err = url.JoinPath(lcpsv, "contents", pub.UUID)
+	}
+	if err != nil {
+		return err
+	}
 
-		var publication PublicationV2
-		publication.UUID = pub.ContentID
-		publication.Title = pub.FileName // the filename is sent, replaces a good title
-		publication.EncryptionKey = pub.ContentKey
-		publication.Location = pub.Output // url
-		publication.ContentType = pub.ContentType
-		publication.Size = uint32(pub.Size)
-		publication.Checksum = pub.Checksum
+	// look for the username and password in the url
+	err = getUsernamePassword(&notifyURL, &username, &password)
+	if err != nil {
+		return err
+	}
 
-		jsonBody, err = json.Marshal(publication)
+	var req *http.Request
+	var jsonBody []byte
+
+	// the payload sent to the server differs from v1 to v2 servers
+	if !v2 {
+
+		var msg apilcp.Encrypted
+		msg.ContentID = pub.UUID
+		msg.ContentKey = pub.EncryptionKey
+		msg.StorageMode = pub.StorageMode
+		msg.Output = pub.Location
+		msg.FileName = pub.FileName
+		msg.ContentType = pub.ContentType
+		msg.Size = int64(pub.Size)
+		msg.Checksum = pub.Checksum
+
+		jsonBody, err = json.Marshal(msg)
 		if err != nil {
 			return err
 		}
-		req, err = http.NewRequest("POST", url, bytes.NewReader(jsonBody))
+		req, err = http.NewRequest("PUT", notifyURL, bytes.NewReader(jsonBody))
+		if err != nil {
+			return err
+		}
+	} else {
+		var msg LCPServerMsgV2
+		msg.UUID = pub.UUID
+		msg.Title = pub.Title
+		msg.EncryptionKey = pub.EncryptionKey
+		msg.Location = pub.Location
+		msg.ContentType = pub.ContentType
+		msg.Size = pub.Size
+		msg.Checksum = pub.Checksum
+
+		jsonBody, err = json.Marshal(msg)
+		if err != nil {
+			return err
+		}
+		req, err = http.NewRequest("POST", notifyURL, bytes.NewReader(jsonBody))
 		if err != nil {
 			return err
 		}
@@ -95,6 +141,142 @@ func NotifyLcpServer(pub *apilcp.LcpPublication, notifyURL string, username stri
 	if (resp.StatusCode != 302) && (resp.StatusCode/100) != 2 { //302=found or 20x reply = OK
 		return fmt.Errorf("the server returned an error %d", resp.StatusCode)
 	}
-	fmt.Println("The server was notified")
+	fmt.Println(notifyURL + " was notified that a new publication was encrypted")
+	return nil
+}
+
+// AbortNotification rolls back the notification of the encryption of a publication
+func AbortNotification(pub Publication, lcpsv string, v2 bool, username string, password string) error {
+
+	// An empty notify URL is not an error
+	if lcpsv == "" {
+		return nil
+	}
+
+	if !strings.HasPrefix(lcpsv, "http://") && !strings.HasPrefix(lcpsv, "https://") {
+		lcpsv = "http://" + lcpsv
+	}
+	var notifyURL string
+	var err error
+	if v2 {
+		notifyURL, err = url.JoinPath(lcpsv, "publications")
+	} else {
+		notifyURL, err = url.JoinPath(lcpsv, "contents", pub.UUID)
+	}
+	if err != nil {
+		return err
+	}
+
+	// look for the username and password in the url
+	err = getUsernamePassword(&notifyURL, &username, &password)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("DELETE", notifyURL, nil)
+	if err != nil {
+		return err
+	}
+
+	req.SetBasicAuth(username, password)
+	client := &http.Client{
+		Timeout: 15 * time.Second,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	if (resp.StatusCode != 302) && (resp.StatusCode/100) != 2 { //302=found or 20x reply = OK
+		return fmt.Errorf("the server returned an error %d", resp.StatusCode)
+	}
+	fmt.Println("Encrypted publication deleted from the LCP Server")
+	return nil
+}
+
+// NotifyCMS notifies a CMS of the encryption of a publication
+func NotifyCMS(pub Publication, notifyURL string) error {
+
+	// An empty notify URL is not an error, simply a silent encryption
+	if notifyURL == "" {
+		return nil
+	}
+
+	// look for the clientID and clientSecret in the url
+	var username, password string
+	err := getUsernamePassword(&notifyURL, &username, &password)
+	if err != nil {
+		return err
+	}
+
+	// set the message sent to the CMS
+	var msg CMSMsg
+	msg.UUID = pub.UUID
+	msg.Title = pub.Title
+	msg.DatePublication = pub.Date
+	msg.Description = pub.Description
+	msg.CoverUrl = pub.CoverUrl
+	var lg Coded
+	for _, v := range pub.Language {
+		lg.Code = v
+		msg.Language = append(msg.Language, lg)
+	}
+	var en Entity
+	for _, en.Name = range pub.Publisher {
+		msg.Publisher = append(msg.Publisher, en)
+	}
+	for _, en.Name = range pub.Author {
+		msg.Author = append(msg.Author, en)
+	}
+	for _, en.Name = range pub.Subject {
+		msg.Category = append(msg.Category, en)
+	}
+
+	var req *http.Request
+	var jsonBody []byte
+
+	jsonBody, err = json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	req, err = http.NewRequest("POST", notifyURL, bytes.NewReader(jsonBody))
+	if err != nil {
+		return err
+	}
+
+	// write a json message to stdout for debug purpose
+	fmt.Println("CMS Notification:")
+	var out bytes.Buffer
+	json.Indent(&out, jsonBody, "", " ")
+	out.WriteTo(os.Stdout)
+
+	req.SetBasicAuth(username, password)
+	client := &http.Client{
+		Timeout: 15 * time.Second,
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	if (resp.StatusCode != 302) && (resp.StatusCode/100) != 2 { //302=found or 20x reply = OK
+		return fmt.Errorf("the server returned an error %d", resp.StatusCode)
+	}
+	fmt.Println(notifyURL + " was notified that a new publication was encrypted")
+	return nil
+}
+
+// Look for the username and password in the url
+func getUsernamePassword(notifyURL, username, password *string) error {
+	u, err := url.Parse(*notifyURL)
+	if err != nil {
+		return err
+	}
+	un := u.User.Username()
+	pw, pwfound := u.User.Password()
+	if un != "" && pwfound {
+		*username = un
+		*password = pw
+		u.User = nil
+		*notifyURL = u.String() // notifyURL is updated
+	}
 	return nil
 }
--- lcpencrypt/lcpencrypt.go
+++ lcpencrypt/lcpencrypt.go
@@ -19,19 +19,20 @@ import (
 func showHelpAndExit() {
 
 	fmt.Println("lcpencrypt encrypts a publication using the LCP DRM")
-	fmt.Println("-input        source epub/pdf/lpf file locator (file system or http GET)")
-	fmt.Println("[-contentid]  optional, content identifier; if omitted a uuid is generated")
-	fmt.Println("[-storage]    optional, target location of the encrypted publication, without filename. File system path or s3 bucket")
-	fmt.Println("[-url]        optional, base url associated with the storage, without filename")
-	fmt.Println("[-filename]   optional, file name of the encrypted publication; if omitted, contentid is used")
-	fmt.Println("[-output]     optional, target folder of encrypted publications")
-	fmt.Println("[-temp]       optional, working folder for temporary files")
-	fmt.Println("[-contentkey]  optional, base64 encoded content key; if omitted a random content key is generated")
-	fmt.Println("[-lcpsv]      optional, URL, notification endpoint for the License server v1")
-	fmt.Println("[-notify]     optional, URL, notification endpoint for the PubStore or License server v2")
-	fmt.Println("[-login]      login (License server) ")
-	fmt.Println("[-password]   password (License server)")
-	fmt.Println("[-help] :     help information")
+	fmt.Println("-input      source epub/pdf/lpf file locator (file system or http GET)")
+	fmt.Println("-contentid  optional, content identifier; if omitted a uuid is generated")
+	fmt.Println("-storage    optional, target location of the encrypted publication, without filename. File system path or s3 bucket")
+	fmt.Println("-url        optional, base url associated with the storage, without filename")
+	fmt.Println("-filename   optional, file name of the encrypted publication; if omitted, contentid is used")
+	fmt.Println("-output     optional, target folder of encrypted publications")
+	fmt.Println("-temp       optional, working folder for temporary files")
+	fmt.Println("-contentkey optional, base64 encoded content key; if omitted a random content key is generated")
+	fmt.Println("-lcpsv      optional, URL, host name of the License server which is notified; the preferred syntax is http://username:password@example.com")
+	fmt.Println("-v2         optional, boolean, indicates a v2 License server")
+	fmt.Println("-login      optional, used along with lcpsv, username for the License server")
+	fmt.Println("-password   optional, used along with lcpsv, password for the License server")
+	fmt.Println("-notify     optional, URL, notification endpoint for a CMS; its syntax is http://username:password@example.com")
+	fmt.Println("-help :     help information")
 	os.Exit(0)
 }
 
@@ -51,10 +52,11 @@ func main() {
 	outputRepo := flag.String("output", "", "optional, target folder of encrypted publications")
 	tempRepo := flag.String("temp", "", "optional, working folder for temporary files")
 	contentkey := flag.String("contentkey", "", "optional, base64 encoded content key; if omitted a random content key is generated")
-	lcpsv := flag.String("lcpsv", "", "optional, http endpoint, notification of the License server")
-	notify := flag.String("notify", "", "optional, notification endpoint")
-	username := flag.String("login", "", "login (License server)")
-	password := flag.String("password", "", "password (License server)")
+	lcpsv := flag.String("lcpsv", "", "URL, host name of the License server which is notified; the preferred syntax is http://username:password@example.com")
+	v2 := flag.Bool("v2", false, "optional, boolean, indicates a v2 License serve")
+	username := flag.String("login", "", "optional unless lcpsv is used, username for the License server")
+	password := flag.String("password", "", "optional unless lcpsv is used, password for the License server")
+	notify := flag.String("notify", "", "optional, URL, notification endpoint for a CMS; its syntax is http://username:password@example.com")
 
 	help := flag.Bool("help", false, "shows information")
 
@@ -66,9 +68,6 @@ func main() {
 		showHelpAndExit()
 	}
 
-	if *lcpsv != "" && (*username == "" || *password == "") {
-		exitWithError("Parameters", errors.New("incorrect parameters, lcpsv needs a login and password, for more information type 'lcpencrypt -help' "))
-	}
 	if *storageRepo != "" && *storageURL == "" {
 		exitWithError("Parameters", errors.New("incorrect parameters, storage requires url, for more information type 'lcpencrypt -help' "))
 	}
@@ -82,24 +81,28 @@ func main() {
 	start := time.Now()
 
 	// encrypt the publication
-	pub, err := encrypt.ProcessEncryption(*contentid, *contentkey, *inputPath, *tempRepo, *outputRepo, *storageRepo, *storageURL, *storageFilename)
+	publication, err := encrypt.ProcessEncryption(*contentid, *contentkey, *inputPath, *tempRepo, *outputRepo, *storageRepo, *storageURL, *storageFilename)
 	if err != nil {
 		exitWithError("Error processing a publication", err)
 	}
 
 	elapsed := time.Since(start)
 
-	// notify the license server or PubStore
-	// lcpsv has priority
-	v2 := false
-	if *lcpsv != "" {
-		notify = lcpsv
-	} else if *notify != "" {
-		v2 = true
-	}
-	err = encrypt.NotifyLcpServer(pub, *notify, *username, *password, v2)
+	// notify the license server
+	err = encrypt.NotifyLCPServer(*publication, *lcpsv, *v2, *username, *password)
 	if err != nil {
-		exitWithError("Notification error", err)
+		exitWithError("Error notifying the LCP Server", err)
+	}
+
+	// notify a CMS (v2 syntax; username and password are always in the URL)
+	err = encrypt.NotifyCMS(*publication, *notify)
+	if err != nil {
+		fmt.Println("Error notifying the CMS:", err.Error())
+		// abort the notification of the license server
+		err = encrypt.AbortNotification(*publication, *lcpsv, *v2, *username, *password)
+		if err != nil {
+			exitWithError("Error aborting notification of the LCP Server", err)
+		}
 	}
 
 	fmt.Println("The encryption took", elapsed)
