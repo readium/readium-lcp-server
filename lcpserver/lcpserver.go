3c5ea03fafafb66e438235f9d312d51a4237ac43
Laurent Le Meur <laurent.lemeur@edrlab.org>
2025-12-18 10:45:24 +0100

    Configure database connection pool

--- lcpserver/lcpserver.go
+++ lcpserver/lcpserver.go
@@ -15,6 +15,7 @@ import (
 	"strconv"
 	"strings"
 	"syscall"
+	"time"
 
 	auth "github.com/abbot/go-http-auth"
 	_ "github.com/go-sql-driver/mysql"
@@ -87,6 +88,12 @@ func main() {
 		log.Println("Error opening the sql db: " + err.Error())
 		os.Exit(1)
 	}
+
+	// Configure database connection pool
+	db.SetMaxOpenConns(25)                 // Limit maximum concurrent connections
+	db.SetMaxIdleConns(10)                 // Keep 10 connections ready for reuse
+	db.SetConnMaxLifetime(5 * time.Minute) // Recycle connections every 5 minutes
+	db.SetConnMaxIdleTime(2 * time.Minute) // Close idle connections after 2 minutes
 
 	if driver == "sqlite3" && !strings.Contains(cnxn, "_journal") {
 		_, err = db.Exec("PRAGMA journal_mode = WAL")
