832700b94009a2f177238c875166bc081a9eafb5
Laurent Le Meur <laurent.lemeur@edrlab.org>
2026-01-12 19:55:12 +0100

    file renamed

--- lcpencrypt/get_content_key.go
+++ lcpencrypt/get_content_key.go
@@ -16,22 +16,24 @@ import (
 )
 
 type ContentInfo struct {
-	ID            string `json:"id"`
 	EncryptionKey []byte `json:"key,omitempty"`
-	Location      string `json:"location"`
-	Length        int64  `json:"length"`
-	Sha256        string `json:"sha256"`
-	Type          string `json:"type"`
 }
+
+type PubInfo struct {
+	EncryptionKey []byte    `json:"encryption_key"`
+}
+
 
 // getContentKey gets content information from the License Server
 // for a given content id,
 // and returns the associated content key.
-func getContentKey(contentKey *string, contentID, lcpsv string, v2 bool, username, password string) error {
+func getContentKey(contentID, lcpsv string, v2 bool, username, password string) (string, error) {
+
+	var contentKey string
 
 	// An empty notify URL is not an error, simply a silent encryption
 	if lcpsv == "" {
-		return nil
+		return contentKey, nil
 	}
 
 	if !strings.HasPrefix(lcpsv, "http://") && !strings.HasPrefix(lcpsv, "https://") {
@@ -40,23 +42,23 @@ func getContentKey(contentKey *string, contentID, lcpsv string, v2 bool, usernam
 	var getInfoURL string
 	var err error
 	if v2 {
-		getInfoURL, err = url.JoinPath(lcpsv, "publications", contentID, "info")
+		getInfoURL, err = url.JoinPath(lcpsv, "publications", contentID)
 	} else {
 		getInfoURL, err = url.JoinPath(lcpsv, "contents", contentID, "info")
 	}
 	if err != nil {
-		return err
+		return contentKey, nil
 	}
 
 	// look for the username and password in the url
 	err = getUsernamePassword(&getInfoURL, &username, &password)
 	if err != nil {
-		return err
+		return contentKey, nil
 	}
 
 	req, err := http.NewRequest("GET", getInfoURL, nil)
 	if err != nil {
-		return err
+		return contentKey, nil
 	}
 
 	req.SetBasicAuth(username, password)
@@ -65,23 +67,35 @@ func getContentKey(contentKey *string, contentID, lcpsv string, v2 bool, usernam
 	}
 	resp, err := client.Do(req)
 	if err != nil {
-		return err
+		return contentKey, nil
 	}
 	defer resp.Body.Close()
 
 	// if the content is found, the content key is updated
 	if resp.StatusCode == http.StatusOK {
-		contentInfo := ContentInfo{}
-		dec := json.NewDecoder(resp.Body)
-		err = dec.Decode(&contentInfo)
-		if err != nil {
-			return errors.New("unable to decode content information")
-		}
 
-		*contentKey = b64.StdEncoding.EncodeToString(contentInfo.EncryptionKey)
+		if v2 {
+			pubInfo := PubInfo{}
+			dec := json.NewDecoder(resp.Body)
+			err = dec.Decode(&pubInfo)
+			if err != nil {
+				return contentKey, errors.New("unable to decode content information")
+			}
+			contentKey = b64.StdEncoding.EncodeToString(pubInfo.EncryptionKey)
+		} else {
+			contentInfo := ContentInfo{}
+			dec := json.NewDecoder(resp.Body)
+			err = dec.Decode(&contentInfo)
+			if err != nil {
+				return contentKey, errors.New("unable to decode content information")
+			}
+			contentKey = b64.StdEncoding.EncodeToString(contentInfo.EncryptionKey)
+		}
 		fmt.Println("Existing encryption key retrieved")
+	} else if resp.StatusCode != http.StatusNotFound {
+		return contentKey, fmt.Errorf("the server returned an error %d", resp.StatusCode)
 	}
-	return nil
+	return contentKey, nil
 }
 
 // Look for the username and password in the url
